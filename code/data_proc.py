a4397024df5749cf6c927511cd7913a0162b2611
jacopoaltieri <jacopo.altieri@gmail.com>
2024-11-08 10:00:28 +0100

    added resizer flag

--- code/data_proc.py
+++ code/data_proc.py
@@ -47,7 +47,37 @@ def process_image(file_path, output_folder, roi_size, process_all_frames=False):
                 print(f"Saved {output_file_path}")
 
 
-def process_folder(input_folder, output_folder, roi_size, max_workers, process_all_frames=False):
+def rescale_image(file_path, output_folder, target_shape, process_all_frames=False):
+    # Read the multi-page TIFF image
+    tiff = cv2.imreadmulti(file_path, [], cv2.IMREAD_UNCHANGED)
+
+    if not tiff[0]:
+        print(f"Failed to read {file_path}")
+        return
+
+    frames = tiff[1]
+    num_frames = len(frames)
+
+    # Determine frames to process
+    frames_to_process = (
+        [frames[num_frames // 2]] if not process_all_frames else frames
+    )
+
+    for frame_index, frame in enumerate(frames_to_process):
+        # Resize the frame to the target shape
+        resized_frame = cv2.resize(frame, target_shape, interpolation=cv2.INTER_LINEAR)
+
+        # Construct the output file name with frame index
+        frame_suffix = frame_index + 1 if process_all_frames else "middle"
+        output_file_name = f"{os.path.splitext(os.path.basename(file_path))[0]}_frame_{frame_suffix}_rescaled.png"
+        output_file_path = os.path.join(output_folder, output_file_name)
+
+        # Save the resized frame as a 16-bit PNG
+        cv2.imwrite(output_file_path, resized_frame)
+        print(f"Saved {output_file_path}")
+
+
+def process_folder(input_folder, output_folder, img_size, max_workers, process_all_frames=False, mode="roi"):
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
@@ -55,11 +85,13 @@ def process_folder(input_folder, output_folder, roi_size, max_workers, process_a
 
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
         for file_path in files:
-            executor.submit(process_image, file_path, output_folder, roi_size, process_all_frames)
+            if mode == "roi":
+                executor.submit(process_image, file_path, output_folder, img_size, process_all_frames)
+            elif mode == "rescale":
+                executor.submit(rescale_image, file_path, output_folder, img_size, process_all_frames)
 
 
 if __name__ == "__main__":
-    
     CFG_PATH = r"./cfg_512.yaml"
     
     num_cores = os.cpu_count()
@@ -71,5 +103,6 @@ if __name__ == "__main__":
     output_folder = cfg["dataset"]["directory"]
     img_size = tuple(cfg["dataset"]["input_size"])
     process_all_frames = cfg["dataset"].get("process_all_frames", False)
+    mode = cfg["dataset"].get("mode", "roi")  # Default to "roi" but can be "rescale"
 
-    process_folder(input_folder, output_folder, img_size, max_workers, process_all_frames)
+    process_folder(input_folder, output_folder, img_size, max_workers, process_all_frames, mode)
