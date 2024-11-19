4940fa39bb0c241ed7d12c1c93fa3e0c7d6d473c
jacopoaltieri <jacopo.altieri@gmail.com>
2024-11-19 09:58:24 +0100

    inplace ops for inference

--- code/models.py
+++ code/models.py
@@ -124,42 +124,6 @@ class ResNetBlock(nn.Module):
         return x + h
 
 
-# class AttnBlock(nn.Module):
-#     def __init__(self, in_ch):
-#         super().__init__()
-#         self.in_ch = in_ch
-
-#         self.norm = Normalize(in_ch)
-#         self.q = self.k =self.v = torch.nn.Conv2d(in_ch, in_ch, kernel_size=1, stride=1, padding=0)
-
-#         self.proj_out = torch.nn.Conv2d(in_ch, in_ch, kernel_size=1, stride=1, padding=0)
-
-#     def forward(self, x):
-#         h_ = self.norm(x)
-#         q = self.q(h_)
-#         k = self.k(h_)
-#         v = self.v(h_)
-
-#         # compute attention
-#         b, c, h, w = q.shape
-#         q = q.reshape(b, c, h * w)
-#         q = q.permute(0, 2, 1)  # b,hw,c
-#         k = k.reshape(b, c, h * w)  # b,c,hw
-#         w_ = torch.bmm(q, k)  # b,hw,hw    w[b,i,j]=sum_c q[b,i,c]k[b,c,j]
-#         w_ = w_ * (int(c) ** (-0.5))
-#         w_ = torch.nn.functional.softmax(w_, dim=2)
-
-#         # attend to values
-#         v = v.reshape(b, c, h * w)
-#         w_ = w_.permute(0, 2, 1)  # b,hw,hw (first hw of k, second of q)
-#         # b, c,hw (hw of q) h_[b,c,j] = sum_i v[b,c,i] w_[b,i,j]
-#         h_ = torch.bmm(v, w_)
-#         h_ = h_.reshape(b, c, h, w)
-
-#         h_ = self.proj_out(h_)
-
-#         return x + h_
-
 
 class AttnBlock(nn.Module): # Simplified Channel Attention
     def __init__(self, in_ch):
@@ -320,7 +284,7 @@ class UNet(nn.Module):
         h = self.norm_out(h)
         h = self.silu(h)
         return self.conv_out(h)
-    
+
     
 if __name__ == "__main__":
     def calculate_model_size(model):
